418596498b27d84f2255aff40fe08a0b7f968678
k-yukio <yuki.on.it@gmail.com>
2024-10-02 20:10:52 +0900

    18

--- application.py
+++ application.py
@@ -9,8 +9,11 @@ class Application(tk.Frame):
         self.atelier = Atelier(master)
         self.field = Field()
         self.tetromino = Tetromino(7, 1)
+        self.controller = {"x": 0, "y": 0}
+        master.bind("<KeyPress>", self.key_event)
 
     def update(self):
+        self.control_proc()
         self.drop_proc()
         self.atelier.delete_all()
         self.atelier.draw_field(self.field)
@@ -26,6 +29,24 @@ class Application(tk.Frame):
             for block in self.tetromino.calc_blocks():
                 self.field.put_block(block.x, block.y, self.tetromino.shape)
             self.tetromino = Tetromino(7, 1)
+
+    def control_proc(self):
+        future_tetromino = self.tetromino.copy()
+        future_tetromino.x += self.controller["x"]
+        future_tetromino.y += self.controller["y"]
+        if self.field.is_allowed(future_tetromino):
+            self.tetromino.x += self.controller["x"]
+            self.tetromino.y += self.controller["y"]
+        self.controller = {"x": 0, "y": 0}
+
+    def key_event(self, event):
+        key = event.keysym
+        if key == "1":
+            self.controller = {"x": -1, "y": 0}
+        if key == "2":
+            self.controller = {"x": 0, "y": 1}
+        if key == "3":
+            self.controller = {"x": 1, "y": 0}
 
 
 if __name__ == "__main__":
