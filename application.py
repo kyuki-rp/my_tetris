74d7c3e33091b5356e0a9307afa86776949a236c
k-yukio <yuki.on.it@gmail.com>
2024-10-02 19:46:11 +0900

    5

--- application.py
+++ application.py
@@ -7,10 +7,13 @@ class Application(tk.Frame):
         master.title("マイテトリス")
         self.canvas = tk.Canvas(master, width=400, height=400, bg="skyblue")
         self.canvas.pack()
-        self.draw_block()
+        self.block_size = 20
+        self.draw_block(0, 0)
 
-    def draw_block(self):
-        self.canvas.create_rectangle(0, 0, 20, 20, fill="grey")
+    def draw_block(self, x, y):
+        self.canvas.create_rectangle(
+            x, y, x + self.block_size, y + self.block_size, fill="grey"
+        )
 
 
 if __name__ == "__main__":
