36aabb1edcf9ab576fb86ca6a8e4ff3c6620f9f7
k-yukio <yuki.on.it@gmail.com>
2024-10-02 19:41:26 +0900

    3

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ import tkinter as tk
 class Application(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
+        master.title("マイテトリス")
+        self.canvas = tk.Canvas(master, width=400, height=400, bg="skyblue")
+        self.canvas.pack()
 
 
 if __name__ == "__main__":
