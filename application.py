731739793cfe49de1187e52b7279cea10f93e7df
k-yukio <yuki.on.it@gmail.com>
2024-10-02 19:48:45 +0900

    8

--- application.py
+++ application.py
@@ -8,9 +8,10 @@ class Application(tk.Frame):
         self.canvas = tk.Canvas(master, width=400, height=400, bg="skyblue")
         self.canvas.pack()
         self.block_size = 20
-        for x in range(0, 200, 20):
-            self.draw_block(x, 0)
-            
+        for y in range(0, 200, 20):
+            for x in range(0, 200, 20):
+                self.draw_block(x, y)
+
     def draw_block(self, x, y):
         self.canvas.create_rectangle(
             x, y, x + self.block_size, y + self.block_size, fill="grey"
